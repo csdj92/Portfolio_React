7054399254e94f464eff428dba1d90b6b6f5870e
Cj Davis <csdj92@gmail.com>
2021-04-28 07:52:25 -0400

    Added testimonials

---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,20 @@
+const testimonials = [
+    {
+      id: 1,
+      name: 'My Bird "Kiwi"',
+      title: 'Guard Bird',
+      org: 'Neighbourhood watch',
+      desc:
+        'Charles is a great guy, especially when he gives me treats.',
+    },
+    {
+      id: 2,
+      name: 'Shirly Davis',
+      title: 'Grandma',
+      org: 'Retired',
+      desc:
+        'I love Charles like he is my grandson.',
+    },
+    
+  ];
+  export default testimonials;
